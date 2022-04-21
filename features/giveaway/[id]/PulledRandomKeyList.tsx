4ad6c607f2ba82bb31304230c1441f9b54a5722b
Phan Dung Tri <phandungtri99@gmail.com>
2022-04-21 14:06:32 +0700

    update: add empty text for pulled key list

--- features/giveaway/[id]/PulledRandomKeyList.tsx
+++ features/giveaway/[id]/PulledRandomKeyList.tsx
@@ -1,5 +1,5 @@
 import { Icon } from "@iconify/react";
-import { Card, List, Title } from "@mantine/core";
+import { Card, List, Text, Title } from "@mantine/core";
 
 type Props = {
 	keys: string[];
@@ -11,11 +11,17 @@ export const PulledRandomKeyList = ({ keys }: Props): JSX.Element => {
 			<Title order={5} mb="xs">
 				&bull; Pulled keys:
 			</Title>
-			<List size="sm" mt="xs" icon={<Icon icon="bx:key" inline />} spacing="xs" sx={{ userSelect: "none" }}>
-				{keys.map((str, i) => (
-					<List.Item key={i}>{str}</List.Item>
-				))}
-			</List>
+			{keys.length === 0 ? (
+				<Text size="xs" color="dimmed">
+					You didn&apos;t pull any key yet.
+				</Text>
+			) : (
+				<List size="sm" mt="xs" icon={<Icon icon="bx:key" inline />} spacing="xs" sx={{ userSelect: "none" }}>
+					{keys.map((str, i) => (
+						<List.Item key={i}>{str}</List.Item>
+					))}
+				</List>
+			)}
 		</Card>
 	);
 };
